9d6a7b4ef5ebc83ec39dca654de620390e887bdb
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-06-29 11:29:14 -0400

    Work on css layouy

---
+++ src/AudioData.js
@@ -0,0 +1,53 @@
+const AudioData = [
+{
+    keyTrigger: 'Q',
+    id: 'audio-1',
+    url: ''
+},
+{
+    keyTrigger: 'W',
+    id: 'audio-2',
+    url: ''
+},
+{
+    keyTrigger: 'E',
+    id: 'audio-3',
+    url: ''
+},
+{
+    keyTrigger: 'A',
+    id: 'audio-4',
+    url: ''
+},
+{
+    keyTrigger: 'S',
+    id: 'audio-5',
+    url: ''
+},
+{
+    keyTrigger: 'D',
+    id: 'audio-6',
+    url: ''
+},
+{
+    keyTrigger: 'Z',
+    id: 'audio-7',
+    url: ''
+},
+{
+    keyTrigger: 'X',
+    id: 'audio-8',
+    url: ''
+},
+{
+    keyTrigger: 'C',
+    id: 'audio-9',
+    url: ''
+},
+]
+
+
+
+
+ 
+export default AudioData;
--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,47 +1,48 @@
 import './index.scss'
 import React, { useState} from 'react';
+import AudioData from './AudioData';
 
 const DrumMachine = () => {
     return ( 
         <div id="drum-machine">
         <div id="inner-drum-machine-box">
             <div className="row">
-                <div id="Q" className="col">
-                    Q
+                <div className="col">
+                    <button id="audio-1" className="drum-pad">Q</button>
                 </div>
-                <div id="W" className="col">
-                    W
+                <div className="col">
+                <button id="audio-2" className="drum-pad">W</button>
                 </div>
-                <div id="E" className="col">
-                    E
+                <div className="col">
+                <button id="audio-3" className="drum-pad">E</button>
                 </div>
                 <div className="col-5">
                     TOP ROW OPEN SPACE
                 </div>
             </div>
             <div className="row">
-                <div id="A" className="col">
-                    A
+                <div className="col">
+                <button id="audio-4" className="drum-pad">A</button>
                 </div>
-                <div id="S" className="col">
-                    S
+                <div className="col">
+                <button id="audio-5" className="drum-pad">S</button>
                 </div>
-                <div id="D" className="col">
-                    D
+                <div className="col">
+                <button id="audio-6" className="drum-pad">D</button>
                 </div>
                 <div id="display" className="col-5">
                     DISPLAY
                 </div>
             </div>
             <div className="row">
-                <div id="Z" className="col">
-                    Z
+                <div className="col">
+                <button id="audio-7" className="drum-pad">Z</button>
                 </div>
-                <div id="X" className="col">
-                    X
+                <div className="col">
+                <button id="audio-8" className="drum-pad">X</button>
                 </div>
-                <div id="C" className="col">
-                    C
+                <div className="col">
+                <button id="audio-9" className="drum-pad">C</button>
                 </div>
                 <div className="col-5">
                 BOTTOM ROW OPEN SPACE
