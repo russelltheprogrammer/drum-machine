db33d6f5e697d46b95cbc626698143c9810cb309
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-07-14 15:52:30 -0400

    Working on passing FCC test- extra div for drum-pad

--- src/AudioButton.js
+++ src/AudioButton.js
@@ -5,7 +5,7 @@ const AudioButton = ({ letter, props, audioElement }) => {
 
     return ( 
         <div>
-            <div id={props.audioId} className="drum-pad">
+            <div id={props.audioId} className="audio-button">
                 <audio ref={audioElement} id={letter} className="clip" src={props.url} preload="preload"></audio>
                 <span>{letter}</span>
             </div> 
